fe886cf7442c1b803026e06b4934fb5a672289ac
Phil Adams <phil_adams@us.ibm.com>
2021-01-27 13:52:50 -0600

    build: fix logic in publishCodeCoverage.sh [skip ci]

--- build/publishCodeCoverage.sh
+++ build/publishCodeCoverage.sh
@@ -3,10 +3,9 @@
 # This script will publish code coverage info for a build of the master branch
 # or a tagged release.
 
-if [[ -n "${TRAVIS_TAG}"  || ${TRAVIS_BRANCH} == "master" ]]; then
+if [[ -n "${TRAVIS_TAG}" || "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     printf ">>>>> Publishing code coverage info for branch: %s\n" ${TRAVIS_BRANCH}
     $HOME/codecov-bash.sh -f coverage.txt -t $CODECOV_TOKEN
 else
     printf ">>>>> Bypassing code coverage publish step for feature branch/PR build.\n"
 fi
-     
