f0796a390828ca443809b17f41764acf0b901e47
András Csányi <andras.csanyi@pm.me>
2021-03-24 20:04:37 +0100

    fix(usage reports): fix labels in example output

--- usagereportsv4/usage_reports_v4_examples_test.go
+++ usagereportsv4/usage_reports_v4_examples_test.go
@@ -25,6 +25,8 @@ import (
 
 	"github.com/IBM/go-sdk-core/v4/core"
 	"github.com/IBM/platform-services-go-sdk/usagereportsv4"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 //
@@ -134,7 +136,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(accountSummary, "", "  ")
-			fmt.Printf("\nGetAccountSummary() result:\n %s \n", string(b))
+			fmt.Printf("\nGetAccountSummary() result:\n%s\n", string(b))
 
 			// end-get_account_summary
 
@@ -155,7 +157,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(accountUsage, "", "  ")
-			fmt.Printf("\nGetAccountUsage() result:\n %s \n", string(b))
+			fmt.Printf("\nGetAccountUsage() result:\n%s\n", string(b))
 
 			// end-get_account_usage
 
@@ -177,7 +179,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(resourceGroupUsage, "", "  ")
-			fmt.Printf("\nGetResourceGroupUsage() result:\n %s \n", string(b))
+			fmt.Printf("\nGetResourceGroupUsage() result:\n%s\n", string(b))
 
 			// end-get_resource_group_usage
 
@@ -199,7 +201,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(orgUsage, "", "  ")
-			fmt.Printf("\nGetOrgUsage() result:\n %s \n", string(b))
+			fmt.Printf("\nGetOrgUsage() result:\n%s\n", string(b))
 
 			// end-get_org_usage
 
@@ -220,7 +222,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(instancesUsage, "", "  ")
-			fmt.Printf("\nGetResourceUsageAccount() result:\n %s \n", string(b))
+			fmt.Printf("\nGetResourceUsageAccount() result:\n%s\n", string(b))
 
 			// end-get_resource_usage_account
 
@@ -242,7 +244,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(instancesUsage, "", "  ")
-			fmt.Printf("\nGetResourceUsageResourceGroup() result:\n %s \n", string(b))
+			fmt.Printf("\nGetResourceUsageResourceGroup() result:\n%s\n", string(b))
 
 			// end-get_resource_usage_resource_group
 
@@ -264,7 +266,7 @@ var _ = Describe(`UsageReportsV4 Examples Tests`, func() {
 				panic(err)
 			}
 			b, _ := json.MarshalIndent(instancesUsage, "", "  ")
-			fmt.Printf("\nGetResourceUsageOrg() result:\n %s \n", string(b))
+			fmt.Printf("\nGetResourceUsageOrg() result:\n%s\n", string(b))
 
 			// end-get_resource_usage_org
 
