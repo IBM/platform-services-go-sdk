135b2c89f4848e4321928ea1a058d602221168df
Phil Adams <phil_adams@us.ibm.com>
2021-04-27 16:45:30 -0500

    fix(Catalog Management): re-gen SDK code after recent API changes

--- catalogmanagementv1/catalog_management_v1_suite_test.go
+++ catalogmanagementv1/catalog_management_v1_suite_test.go
@@ -17,9 +17,10 @@
 package catalogmanagementv1_test
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
 func TestCatalogManagementV1(t *testing.T) {
