04c49710bb73bfb63a34d0b3ba4a7fff4430067b
James Dubee <jwdubee@us.ibm.com>
2023-04-27 11:42:14 -0400

    feat(Global Catalog): re-gen service after recent API changes
    Signed-off-by: James Dubee <jwdubee@us.ibm.com>

--- globalcatalogv1/global_catalog_v1_suite_test.go
+++ globalcatalogv1/global_catalog_v1_suite_test.go
@@ -1,5 +1,5 @@
 /**
- * (C) Copyright IBM Corp. 2021.
+ * (C) Copyright IBM Corp. 2023.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +17,10 @@
 package globalcatalogv1_test
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"testing"
 )
 
 func TestGlobalCatalogV1(t *testing.T) {
